87c2a93d87a78f91c8154c029d08f5cbc8749618
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-04-17 09:27:57 -0300

    Add footer

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -12,15 +12,15 @@ function Cover() {
       <div className="cover-btns">
         <Button
           className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
+          buttonStyle="btn-outline"
+          buttonSize="btn-large"
         >
           GET STARTED
         </Button>
         <Button
           className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
+          buttonStyle="btn-primary"
+          buttonSize="btn-large"
         >
           WATCH TRAILER
           <i className="far fa-play-circle" />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import Cover from '../Cover';
 import Cards from '../Cards';
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <Cover />
       <Cards />
+      <Footer />
     </>
   );
 }
